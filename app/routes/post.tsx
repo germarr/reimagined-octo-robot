efffd047f9c10bfec58dccbf53eaa8ad2e02d44d
Gerardo Martínez <you@example.com>
2022-04-11 07:45:44 -0400

    V0.0.1

--- app/routes/post.tsx
+++ app/routes/post.tsx
@@ -1,7 +1,14 @@
 import { Outlet } from "@remix-run/react"
+import hljs from 'highlight.js';
+import styles from "highlight.js/styles/github-dark-dimmed.css"
+import { useEffect } from "react";
 
 export default function Post(){
-
+   
+    useEffect(() => {
+        hljs.initHighlighting();
+    }, [])
+    
     return(
         <div>
             <Outlet/>
